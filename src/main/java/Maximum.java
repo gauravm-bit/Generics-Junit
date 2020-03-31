83419d24dea0dada63279bf90c5d07bc3ce5e841
gauravm-bit <gauravwrk01@gmail.com>
2020-03-31 10:24:25 +0530

    ADDED: Maximum of three integers and test case for them.

--- src/main/java/Maximum.java
+++ src/main/java/Maximum.java
@@ -1,5 +1,17 @@
 public class Maximum {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Maximum generics problem");
-    }
+            public static void main(String[] args){
+                System.out.println("Welcome");
+            }
+
+            public static Integer getMaximumInteger(Integer number1, Integer number2, Integer number3) {
+                Integer maximum = number1;
+
+                if (number2.compareTo(maximum)>0)
+                    maximum = number2;
+                if (number3.compareTo(maximum)>0)
+                    maximum = number3;
+                return maximum;
+            }
 }
+
+
--- src/test/java/MaximumTest.java
+++ src/test/java/MaximumTest.java
@@ -1,2 +1,31 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MaximumTest {
+    Maximum max;
+
+    //TC 1.1
+    @Test
+    public void givenIntegers_WhenMaxAtPositionOne_ShouldReturnMaximum() {
+        max = new Maximum();
+        Integer maximum = Maximum.getMaximumInteger(19,17,15);
+        Assert.assertEquals(19,(int)maximum);
+    }
+
+    //TC 1.2
+    @Test
+    public void givenIntegers_WhenMaxAtPositionTwo_ShouldReturnMaximum() {
+        max = new Maximum();
+        Integer maximum = Maximum.getMaximumInteger(17, 19, 15);
+        Assert.assertEquals(19, (int) maximum);
+    }
+
+    //TC 1.3
+    @Test
+    public void givenIntegers_WhenMaxAtPositionThree_ShouldReturnMaximum() {
+        max = new Maximum();
+        Integer maximum = Maximum.getMaximumInteger(15, 17, 19);
+        Assert.assertEquals(19, (int) maximum);
+    }
+
 }
